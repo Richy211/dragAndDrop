9e9c03f0531669dd4af4fa3184e104f1f46111f8
Ricardo Llanos M <40502949+Richy211@users.noreply.github.com>
2025-01-24 17:32:40 -0300

    Primera subida

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TodosView from './components/TodosView.vue';
+
+</script>
+
+<template>
+
+<TodosView />
+
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/InputNew.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, defineEmits } from "vue";
+
+const text = ref("");
+const emits = defineEmits(["onNewItem"]);
+
+function handleSubmit(){
+    if(text != ''){
+        //llamar a el emit
+        emits("onNewItem", text);
+        text.value = '';
+    }
+}
+
+</script>
+
+<template>
+<form action="" @submit.prevent="handleSubmit">
+    <input type="text" v-model="text">
+</form>
+</template>
+
+<style scoped>
+form{
+    margin-bottom: 10px;
+}
+input{
+    width:100%;
+    block-size: border-box;
+}
+
+</style>
