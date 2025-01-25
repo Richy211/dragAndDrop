ccc71beea8e304d826c5804ee88d8b25f241efb1
Ricardo Llanos M <40502949+Richy211@users.noreply.github.com>
2025-01-25 17:22:35 -0300

    Le dimos persistencia en LocalStorsge y agregamos boton de borrar

---
+++ src/components/EjercicioView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive, watch } from 'vue';
+import InputNew from './InputNew.vue';
+
+let boards = reactive([
+    {
+        id: crypto.randomUUID(),
+        name: "tablero 1",
+        items: [
+            {
+                id:crypto.randomUUID(),
+                title:"Feature de archivos",
+            },
+            {
+                id:crypto.randomUUID(),
+                title: "Resolver bug",
+            },
+        ],
+    }
+]);
+
+//Guardar los datos en el local Storage cada vez que cambien
+watch(
+    () => boards,(newBoards) => {
+        localStorage.setItem('boards', JSON.stringify(newBoards));        
+    },
+    { deep: true }
+);
+
+function handleNewItem(text, board) {
+    board.item.push({
+        id: cryptp.randomUUID(),
+        title: text.value,
+    });
+}
+
+ 
+</script>
--- src/components/TodosView.vue
+++ src/components/TodosView.vue
@@ -1,173 +1,182 @@
 <script setup>
-import { reactive } from 'vue';
-import InputNew from './InputNew.vue'; 
+import { reactive, watch } from 'vue';
+import InputNew from './InputNew.vue';
 
-let boards = reactive([
+let boards = reactive(
+  JSON.parse(localStorage.getItem('boards')) || [
     {
-        id: crypto.randomUUID(),
-        name: "tablero 1",
-        items: [
-            {
-                id:crypto.randomUUID(),
-                title: "Feature de archivos",
-            },
-            {
-                id:crypto.randomUUID(),
-                title: "Resolver bug",
-            },
-
-        ],
+      id: crypto.randomUUID(),
+      name: "tablero 1",
+      items: [
+        {
+          id: crypto.randomUUID(),
+          title: "Feature de archivos",
+        },
+        {
+          id: crypto.randomUUID(),
+          title: "Resolver bug",
+        },
+      ],
     },
     {
-        id: crypto.randomUUID(),
-        name: "tablero 2",
-        items: [
-            {
-                id:crypto.randomUUID(),
-                title: "Mandar reporte",
-            },
-            {
-                id:crypto.randomUUID(),
-                title: "Code review",
-            },
-
-        ],
+      id: crypto.randomUUID(),
+      name: "tablero 2",
+      items: [
+        {
+          id: crypto.randomUUID(),
+          title: "Mandar reporte",
+        },
+        {
+          id: crypto.randomUUID(),
+          title: "Code review",
+        },
+      ],
     },
-]);
+  ]
+);
+
+watch(
+  () => boards,
+  (newBoards) => {
+    localStorage.setItem('boards', JSON.stringify(newBoards));
+  },
+  { deep: true }
+);
 
 function handleNewItem(text, board) {
-   /*  console.log(text.value, board.id, board.name); */
-   board.items.push({
+  board.items.push({
     id: crypto.randomUUID(),
     title: text.value,
-   });
+  });
 }
 
 function handleNewBoard() {
-    const name = prompt("Name of the board");
-    if (!!name ) {
-        boards.push({
-            id: crypto.randomUUID(),
-            name: name,
-            items: [],
-        });
-    }
+  const name = prompt("Name of the board");
+  if (name) {
+    boards.push({
+      id: crypto.randomUUID(),
+      name: name,
+      items: [],
+    });
+  }
 }
 
-function startDrag(evt, board, item){
-    evt.dataTransfer.setData(
-        "text/plain",
-        JSON.stringify({ boardId: board.id, itemId: item.id })
-    );
+function startDrag(evt, board, item) {
+  evt.dataTransfer.setData(
+    "text/plain",
+    JSON.stringify({ boardId: board.id, itemId: item.id })
+  );
 }
 
-function onDrop(evt, dest){
-    const { boardId, itemId } = JSON.parse(
-        evt.dataTransfer.getData("text/plain")
-    );
-    /* console.log(boardId, itemId); */
-    const originBoard = boards.find((item) => item.id === boardId);
-    const originItem = originBoard.items.find((item) => item.id === itemId);
+function onDrop(evt, dest) {
+  const { boardId, itemId } = JSON.parse(
+    evt.dataTransfer.getData("text/plain")
+  );
 
-   /*  console.log(originBoard.name, originItem.title); }
-    muestra las referencia deltablero y su titulo*/
+  const originBoard = boards.find((item) => item.id === boardId);
+  const originItem = originBoard.items.find((item) => item.id === itemId);
 
-    dest.items.push({ ...originItem });
-    originBoard.items = originBoard.items.filter((item) => item != originItem);
-
-
-
-
+  dest.items.push({ ...originItem });
+  originBoard.items = originBoard.items.filter((item) => item !== originItem);
 }
 
+function deleteItem(board, item) {
+  board.items = board.items.filter((i) => i.id !== item.id);
+}
 </script>
 
 <template>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="#" @click.prevent="handleNewBoard">Create board</a></li>
-            </ul>
-        </nav>
-    
-        <div class="boards-container">
-            <div class="boards">
-                <div 
-                class="board" 
-                @drop="onDrop($event, board)"
-                @dragover.prevent 
-                @dragenter.prevent 
-                v-for="board in boards" 
-                :key="board.id"
-                >
+  <div>
+    <nav>
+      <ul>
+        <li><a href="#" @click.prevent="handleNewBoard">Create board</a></li>
+      </ul>
+    </nav>
 
-                    <div>{{ board.name }}</div>
-                    <InputNew @on-new-item="(text) => handleNewItem(text, board)" />
-                       
-                <div class="items">
-                    <div class="item"
-                    draggable="true"
-                    @dragstart="startDrag($event, board, item)" 
-                    v-for="item in board.items" 
-                    :key="item.id"
-                    >
-                    {{ item.title }}
-                    </div>
-                </div>
-                </div>
+    <div class="boards-container">
+      <div class="boards">
+        <div 
+          class="board" 
+          @drop="onDrop($event, board)"
+          @dragover.prevent 
+          @dragenter.prevent 
+          v-for="board in boards" 
+          :key="board.id"
+        >
+          <div>{{ board.name }}</div>
+          <InputNew @on-new-item="(text) => handleNewItem(text, board)" />
+          <div class="items">
+            <div
+              class="item"
+              draggable="true"
+              @dragstart="startDrag($event, board, item)"
+              v-for="item in board.items"
+              :key="item.id"
+            >
+              {{ item.title }}
+              <button @click.prevent="deleteItem(board, item)">❌</button>
             </div>
-    
+          </div>
         </div>
-
+      </div>
     </div>
-    
-
-
+  </div>
 </template>
 
 <style scoped>
-
-nav{
-    background-color: black;
-    margin-bottom: 10px;
+nav {
+  background-color: black;
+  margin-bottom: 10px;
 }
 
 nav ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
 }
 
 nav ul li a {
-    display: block;
-    padding: 10px;
-    color: white;
-    text-decoration: none;
+  display: block;
+  padding: 10px;
+  color: white;
+  text-decoration: none;
 }
 
-.boards{
-    display: flex;
-    gap: 10px;
-}
-.board{
-    background-color: #efefef;
-    padding: 10px;
+.boards {
+  display: flex;
+  gap: 10px;
 }
 
-items {
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
+.board {
+  background-color: #efefef;
+  padding: 10px;
 }
 
-.item{
-    background-color: white;
-    padding: 10px;
-    block-size: border-box;
-
+.items {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 }
 
+.item {
+  background-color: white;
+  padding: 10px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+button {
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 5px;
+  cursor: pointer;
+}
 
+button:hover {
+  background-color: darkred;
+}
 </style>
